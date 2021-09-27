2f997be6d73a38012f2109a0236b36710fd2c0b4
aziz <ahmed613@umn.edu>
2021-09-26 23:44:55 -0500

    save/persist data

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,6 +9,7 @@ var hour4 = $("#4");
 var hour5 = $("#5");
 var time = moment();
 
+
 function setSchedule() {
 
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
@@ -21,6 +22,38 @@ function setSchedule() {
             $(this).children(".schedule").val(schedule);
         }
     });
+};
+
+setSchedule();
+
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
 }
+
+pastPresentFuture();
+
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
 
 
