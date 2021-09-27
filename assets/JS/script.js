5868df33e90753870c196a8922e04ab381031b43
aziz <ahmed613@umn.edu>
2021-09-26 23:31:38 -0500

    add variables

---
+++ assets/JS/script.js
@@ -0,0 +1,11 @@
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#1");
+var hour2 = $("#2");
+var hour3 = $("#3");
+var hour4 = $("#4");
+var hour5 = $("#5");
+var time = moment();
+
